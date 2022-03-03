06fe454d485c6b7e47576710937bb7d251f02af5
Ednar Nogueira <ednaredms@gmail.com>
2022-03-03 18:02:33 -0300

    Adicionando métodos getters e setters

---
+++ AplicacaoJava/src/aplicacaojava/Pessoa.java
@@ -0,0 +1,50 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package aplicacaojava;
+
+/**
+ *
+ * @author ednar
+ */
+public class Pessoa {
+    String nome;
+    String Sobrenome;
+    int idade;
+    float peso;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getSobrenome() {
+        return Sobrenome;
+    }
+
+    public void setSobrenome(String Sobrenome) {
+        this.Sobrenome = Sobrenome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public float getPeso() {
+        return peso;
+    }
+
+    public void setPeso(float peso) {
+        this.peso = peso;
+    }
+    
+    
+}
